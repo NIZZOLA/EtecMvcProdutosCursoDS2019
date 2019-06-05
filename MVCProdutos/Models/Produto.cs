041b8d594909eae95a05182e44846743531f5a98
Marcio R Nizzola <marcio@nizzola.com.br>
2019-06-05 20:58:42 -0300

    versao aula 29 05 2019

---
+++ MVCProdutos/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCProdutos.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [MaxLength(50)]
+        public string Descricao { get; set; }
+        [MaxLength(2)]
+        public string UnidadeDeMedida { get; set; }
+        public decimal ValorDeCusto { get; set; }
+
+        public decimal MargemDeLucro { get; set; }
+        public decimal ValorDeVenda { get; set; }
+
+        public DateTime DataDaInclusao { get; set; }
+        public DateTime DataDaAlteracao { get; set; }
+
+        public decimal Estoque { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
